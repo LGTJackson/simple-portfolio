a1574dfc1bc594a533fc584c243ea5a366821fe0
LGTJackson <ljackson1@ithaca.edu>
2023-01-18 21:38:29 -0500

    restructured some files

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -6,7 +6,7 @@ export default [
         details: "As a freelance IT person during the pandemic, I helped people within the Ithaca and Danby communities stay connected by providing support and training them to use new technologies. This included assisting individuals in setting up video conferencing for virtual visits with loved ones, as well as helping remote workers access and use the necessary tools for their jobs. I also provided guidance on cybersecurity best practices to ensure the safety of personal and sensitive information while working online. Overall, my goal was to help the community navigate the challenges of the pandemic by providing reliable IT support.",
     },
     {
-        year: "",
+        year: "2018",
         title: "Job Title",
         duration: "time spent",
         details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
