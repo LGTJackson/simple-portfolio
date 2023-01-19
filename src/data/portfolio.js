b0317acb4635447dbb36762354081f2b7b2ccd8c
LGTJackson <ljackson1@ithaca.edu>
2023-01-18 19:45:13 -0500

    Initial commit for portfolio website I've been working on

---
+++ src/data/portfolio.js
@@ -0,0 +1,27 @@
+export default [
+    /**
+     * JSON objects for projects
+     * @param title Project title
+     * @param imgUrl URL/Path to item's image 
+     * @param Stack List of technologies used in the project
+     * @param link Link to project
+     */
+    {
+        title: "temp1",
+        imgUrl: "public/vite.svg",
+        stack: ["python","django", "asass"],
+        link: "",
+    },
+    {
+        title: "temp2",
+        imgUrl: "public/assets/react.svg",
+        stack: [],
+        link: "",
+    },    
+    {
+        title: "temp3",
+        imgUrl: "",
+        stack: [],
+        link: "",
+    },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: "Today",
+        title: "IT Consultant",
+        duration: "3 Years",
+        details: "After graduating I started doing IT work for elderly communities",
+    },
+    {
+        year: "2019",
+        title: "Job Title",
+        duration: "time spent",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+    {
+        year: "2018",
+        title: "Job Title",
+        duration: "time spent",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    },
+]
